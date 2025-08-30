4a1a9779c6be453374de84b6bb63e113c59c17b6
AshmweGeoffrey <ashimwegeoffrey@gmail.com>
2025-08-30 14:06:55 +0200

    Implement code changes to enhance functionality and improve performance

--- backend/dist/utils/database.d.ts
+++
@@ -1,9 +0,0 @@
-import { PrismaClient } from "@prisma/client";
-declare let prisma: PrismaClient;
-declare global {
-    var __prisma: PrismaClient | undefined;
-}
-export declare function connectDatabase(): Promise<void>;
-export declare function disconnectDatabase(): Promise<void>;
-export { prisma };
-//# sourceMappingURL=database.d.ts.map
--- backend/dist/utils/socketIO.d.ts
+++
@@ -1,3 +0,0 @@
-import { Server } from "socket.io";
-export declare function setupSocketIO(server: any): Server<import("socket.io").DefaultEventsMap, import("socket.io").DefaultEventsMap, import("socket.io").DefaultEventsMap, any>;
-//# sourceMappingURL=socketIO.d.ts.map
